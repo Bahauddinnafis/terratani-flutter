1cb471f48b1d2a69d0a88cf287dbdf2671cd3ecb
Bahauddinnafis <bahauddinafis@gmail.com>
2024-04-02 22:35:15 +0700

    Desing Shop Screen

--- lib/screen/shop_screen.dart
+++ lib/screen/shop_screen.dart
@@ -1,13 +1,159 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ShopScreen extends StatelessWidget {
-  const ShopScreen({super.key});
-  
+  const ShopScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Shop',
-      home: Scaffold()
-      );
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 258,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 0),
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(27, 50, 8, 0),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: const SizedBox(
+                        width: 322,
+                        height: 258,
+                        child: ColoredBox(
+                          color: Color(0xFF219338),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(height: 15),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 27),
+            child: Text(
+              'Kategori',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SizedBox(width: 10), // Padding horizontal
+                  Text(
+                    "Tanaman Panen",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(width: 10), // Padding horizontal
+                  Text(
+                    "Tanaman Hias",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(width: 10), // Padding horizontal
+                  Text(
+                    "Sewa Tanah",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                  SizedBox(width: 10), // Padding horizontal
+                  Text(
+                    "Jual Tanah",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 183,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 0),
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(27, 0, 8, 0),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: const SizedBox(
+                        width: 130,
+                        height: 183,
+                        child: ColoredBox(
+                          color: Color(0xFF219338),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Ada Yang Baru, Nih!",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black),
+            ),
+          ),
+          SizedBox(
+            height: 183,
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 0),
+              scrollDirection: Axis.horizontal,
+              itemCount: 4,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(27, 0, 8, 0),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: const SizedBox(
+                        width: 130,
+                        height: 183,
+                        child: ColoredBox(
+                          color: Color(0xFF219338),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
