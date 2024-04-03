67128fb251864531928c791902534739c5e4231b
Bahauddinnafis <bahauddinafis@gmail.com>
2024-04-03 10:02:28 +0700

    History Screen and Profile Screen

--- lib/screen/history_screen.dart
+++ lib/screen/history_screen.dart
@@ -1,13 +1,54 @@
 import 'package:flutter/material.dart';
 
 class HistoryScreen extends StatelessWidget {
-  const HistoryScreen({super.key});
-  
+  const HistoryScreen({Key? key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'History',
-      home: Scaffold()
-      );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "History",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF219338),
+          ),
+        ),
+      ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => const SizedBox(height: 10),
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.fromLTRB(27, 0, 27, 0),
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  width: double.infinity,
+                  height: 101,
+                  color: Color(0xFF219338),
+                  child: Center(
+                    child: Text(
+                      "Item ${index + 1}",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -1,13 +1,152 @@
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
-  
+  const ProfileScreen({Key? key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Profile',
-      home: Scaffold()
-      );
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              color: Colors.white,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              alignment: Alignment.center,
+              child: Transform.translate(
+                offset: const Offset(0, 8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Edit Profile
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 22),
+                          child: Image.asset(
+                            'assets/images/pen.png',
+                          ),
+                        ),
+                        const Text(
+                          "Edit Profile",
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                    // Change Password
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 22),
+                          child: Image.asset(
+                            'assets/images/lock.png',
+                          ),
+                        ),
+                        const Text(
+                          "Change Password",
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                    // Setting
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 22),
+                          child: Image.asset(
+                            'assets/images/setting.png',
+                          ),
+                        ),
+                        const Text(
+                          "Setting",
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                    // Change email address
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 22),
+                          child: Image.asset(
+                            'assets/images/email.png',
+                          ),
+                        ),
+                        const Text(
+                          "Change Email Address",
+                          style: TextStyle(fontSize: 18, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                    // Logout
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 22),
+                          child: Image.asset(
+                            'assets/images/logout.png',
+                          ),
+                        ),
+                        const Text(
+                          "Logout",
+                          style: TextStyle(fontSize: 18, color: Colors.red),
+                        ),
+                      ],
+                    ),
+                    const Divider(
+                      color: Colors.grey,
+                      thickness: 1,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              height: MediaQuery.of(context).size.height /
+                  3, // Setengah dari tinggi layar
+              child: Container(
+                color: const Color(0xFF219338),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                'assets/images/pattern.png',
+                fit: BoxFit.fill,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
